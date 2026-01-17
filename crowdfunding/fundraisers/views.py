cec86de7332ad0dcea7a505e30ec5394ffc0c803
prakadw-dev <prakasitadewi@gmail.com>
2026-01-17 12:34:05 +1100

    wrote my first DRF view

--- crowdfunding/fundraisers/views.py
+++ crowdfunding/fundraisers/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Fundraiser
+from .serializers import FundraiserSerializer
 
-# Create your views here.
+class FundraiserList(APIView):
+
+    def get(self, request):
+       fundraisers = Fundraiser.objects.all()
+       serializer = FundraiserSerializer(fundraisers, many=True)
+       return Response(serializer.data)
