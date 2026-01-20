93da7efd67ecc19a4e6152c81cef111428547fae
prakadw-dev <prakasitadewi@gmail.com>
2026-01-20 19:37:36 +1100

    finalising fundraisers API

--- crowdfunding/fundraisers/views.py
+++ crowdfunding/fundraisers/views.py
@@ -1,5 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.generics import get_object_or_404
 from .models import Fundraiser
 from .serializers import FundraiserSerializer
 
@@ -9,3 +11,22 @@ class FundraiserList(APIView):
        fundraisers = Fundraiser.objects.all()
        serializer = FundraiserSerializer(fundraisers, many=True)
        return Response(serializer.data)
+    
+    def post(self, request):
+       serializer = FundraiserSerializer(data=request.data)
+       if serializer.is_valid():
+           serializer.save()
+           return Response(
+               serializer.data,
+               status=status.HTTP_201_CREATED
+           )
+       return Response(
+           serializer.errors,
+           status=status.HTTP_400_BAD_REQUEST
+       )
+    
+class FundraiserDetail(APIView):
+   def get(self, request, pk):
+    fundraiser = get_object_or_404(Fundraiser, pk=pk)
+    serializer = FundraiserSerializer(fundraiser)
+    return Response(serializer.data)
