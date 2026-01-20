e4e3939745bfed91dc025e384eed50a7515177b1
prakadw-dev <prakasitadewi@gmail.com>
2026-01-20 20:19:49 +1100

    started Pledge API

--- crowdfunding/fundraisers/views.py
+++ crowdfunding/fundraisers/views.py
@@ -2,8 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.generics import get_object_or_404
-from .models import Fundraiser
-from .serializers import FundraiserSerializer
+from .models import Fundraiser, Pledge
+from .serializers import FundraiserSerializer, PledgeSerializer
 
 class FundraiserList(APIView):
 
@@ -30,3 +30,23 @@ class FundraiserDetail(APIView):
     fundraiser = get_object_or_404(Fundraiser, pk=pk)
     serializer = FundraiserSerializer(fundraiser)
     return Response(serializer.data)
+   
+class PledgeList(APIView):
+
+   def get(self, request):
+       pledges = Pledge.objects.all()
+       serializer = PledgeSerializer(pledges, many=True)
+       return Response(serializer.data)
+
+   def post(self, request):
+       serializer = PledgeSerializer(data=request.data)
+       if serializer.is_valid():
+           serializer.save()
+           return Response(
+               serializer.data,
+               status=status.HTTP_201_CREATED
+           )
+       return Response(
+           serializer.errors,
+           status=status.HTTP_400_BAD_REQUEST
+       )
